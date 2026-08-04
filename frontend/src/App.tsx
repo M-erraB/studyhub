2f3ff0b54d0d628ab577e86ab4dd66b7d9371190
Mustafa Barre <barrespam@Icloud.com>
2026-08-04 17:03:20 -0500

    Set up React app and create Headers.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <Header name = "Mustafa"/>;
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>StudyHub</h1>
+            <h2>Welcome, Mustafa!</h2>
+        </header>
+    )
+}
+export default Header;
